b59e4dde3149050e03f9afc00d2b0064bea6991b
SaucyProgrammer <86332163+SaucyProgrammer@users.noreply.github.com>
2022-05-11 19:17:47 -0400

    Update HomeWork.java
    Modified this file online via GitHub

--- src/main/java/HomeWork.java
+++ src/main/java/HomeWork.java
@@ -5,5 +5,10 @@ public class HomeWork {
         //This is the initial empty class.
 
         System.out.println("Hello Harsh. This is my github practice homework.");
+        
+        
+         System.out.println("I've added this line of code directly from my github online, then I will pull this into my local machine after committing.");
+        
+        
     }
 }
