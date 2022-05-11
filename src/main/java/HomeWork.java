78777e67c8359bcc76d55daa0b6176f1664b922a
SauceyProgrammer <z.darweschzad@gmail.com>
2022-05-11 19:14:29 -0400

    Modified my class file with some code.

--- src/main/java/HomeWork.java
+++ src/main/java/HomeWork.java
@@ -3,5 +3,7 @@ public class HomeWork {
     /** Created on 05-11-2022 **/
     public static void main(String[] args) {
         //This is the initial empty class.
+
+        System.out.println("Hello Harsh. This is my github practice homework.");
     }
 }
